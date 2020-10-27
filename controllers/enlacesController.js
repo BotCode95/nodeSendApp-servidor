801adab31578a681a95ba5b0eeee1666335e08a8
BotCode95 <patribottino95@gmail.com>
2020-10-26 21:33:25 -0300

    solucion nombre de enlace

--- controllers/enlacesController.js
+++ controllers/enlacesController.js
@@ -12,11 +12,13 @@ exports.nuevoEnlace = async (req,res,next) => {
 
     }
 
+    // console.log(req.body);
+
     //creacion objecto enlace
-    const {nombre_original, password} = req.body;
+    const {nombre_original, nombre} = req.body;
     const enlace = new Enlaces();
     enlace.url = shortid.generate();
-    enlace.nombre = shortid.generate();
+    enlace.nombre = nombre;
     enlace.nombre_original = nombre_original;
     
 
